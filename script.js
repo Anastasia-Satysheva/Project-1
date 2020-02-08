6b020ba0004b548fe283eee72899680ce615360c
Anastasia Satysheva <anastasiasatysheva@Anastasias-MacBook-Pro.local>
2020-02-08 10:23:07 -0500

    Changes to Javascript

--- script.js
+++ script.js
@@ -1,4 +1,93 @@
 $(document).ready(function() {
     M.AutoInit();
-    
+    var lat = '';
+var lon = '';
+// var arr;
+ function showlocation(){
+    alert('asd');
+ }
+
+
+    var x = document.getElementById("demo");    
+    function getLocation() {
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+
+
+        const Http = new XMLHttpRequest();
+        // const url='https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200681842-da90e5231d773b9b92835e9dc121c36e';
+        const url='https://www.hikingproject.com/data/get-trails?lat=' + lat +'&lon=' + lon + '&maxDistance=10&key=200681842-da90e5231d773b9b92835e9dc121c36e';
+        // alert(url);
+        Http.open("GET", url);
+        Http.send();
+
+        Http.onreadystatechange = (e) => {
+            // var trail = Http.responseText;
+
+            var trails = JSON.parse(Http.responseText);
+            var arrS = trails['success'];
+
+            var arr = trails['trails'];
+            console.log ( arr.length );
+
+            // alert(arr.length);
+            if(arrS == '1' && arr.length > 0){
+
+                var col = [];
+                for (var i = 0; i < arr.length; i++) {
+                    for (var key in arr[i]) {
+                        if (col.indexOf(key) === -1) {
+                            col.push(key);
+                        }
+                    }
+                }
+
+                var table = document.createElement("table");
+
+                var tr = table.insertRow(-1);                 
+
+                for (var i = 0; i < 9; i++) {
+                    if(col[i] != "id"){
+                        var th = document.createElement("th");    
+                        th.innerHTML = col[i].toUpperCase();
+                        tr.appendChild(th);
+                    }
+                }
+
+                for (var i = 0; i < arr.length; i++) {
+
+                    tr = table.insertRow(-1);
+
+                    for (var j = 0; j < 9; j++) {
+                        if(col[j] != "id"){
+                            var tabCell = tr.insertCell(-1);
+                            if(col[j] == 'url'){
+                                tabCell.innerHTML = "<a href=\"" + arr[i][col[j]] + "\" target=\"_blank\">" + arr[i][col[j]] + "";
+                            }else{
+                                tabCell.innerHTML = arr[i][col[j]];
+                            }
+                        }
+                    }
+                }
+
+                var divContainer = document.getElementById("showData");
+                divContainer.innerHTML = "";
+                divContainer.appendChild(table);
+                            
+            }
+            else{
+                alert ('No data for this location.');
+            }
+
+        }
+
+    }
 });
